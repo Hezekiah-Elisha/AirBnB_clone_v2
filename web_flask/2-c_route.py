5eec14b420011ca37c68cd3776f2b7ea9c1abccb
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-04-13 20:42:58 +0300

    Python is cool!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module hello route of flask"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Print first message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Print route hbnb message"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_ctext(text):
+    """Print a variable rule <text>"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
