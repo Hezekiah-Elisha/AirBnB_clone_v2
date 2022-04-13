c34db174c83c4d4d36d092c27740a159b5ca97d8
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-04-13 21:13:15 +0300

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def show_integern(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def render_templaten(n):
+def render_templatern(n):
     """routes with a template that shows the integer n"""
     return render_template('5-number.html', n=n)
 
